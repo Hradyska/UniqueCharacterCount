29f92ad3ca0b3251cc8ec9639b8b017340128c33
Larysa Hradyska <l.v.hradyska@gmail.com>
2023-03-14 18:21:50 +0200

    adding solution

---
+++ UniqueCharacterCount/UniqueCharacterCount/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+namespace UniqueCharacterCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string quatrian = "The Moving Finger writes; and having writ,\nMoves on: nor all your Piety nor Wit\nShall lure it back to cancel half a Line,\nNor all your Tears wash out a Word of it.";
+            Console.WriteLine(quatrian);
+            var grouped = quatrian.ToLower().ToCharArray().GroupBy(q => q);
+            Console.WriteLine("\ngrouped whith linq");
+            foreach (var group in grouped)
+            {
+                Console.WriteLine(group.Key + " " + group.Count());
+            }
+
+            Dictionary<char, int> keyValuePairs = new Dictionary<char, int>();
+            foreach (char c in quatrian.ToLower())
+            {
+                if (!keyValuePairs.TryAdd(c, 1))
+                {
+                    keyValuePairs[c] += 1;
+                }
+            }
+
+            Console.WriteLine("\ngrouped whith Dictionary");
+            foreach (var c in keyValuePairs)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
